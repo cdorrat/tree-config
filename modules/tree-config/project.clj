06237d11db1d0c46ca217922f3eea513cf7d0da9
Cameron Dorrat <cdorrat@gmail.com>
2014-03-15 16:46:17 +1100

    updated version numbers for initial release

--- modules/tree-config/project.clj
+++ modules/tree-config/project.clj
@@ -1,4 +1,4 @@
-(defproject tree-config "0.1.0-SNAPSHOT"
+(defproject tree-config "0.1.0"
   :description "Hierarchical configuration library for clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
