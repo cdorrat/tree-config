55b7b5b32f408fa7ba84b481e35d79a339926a85
Cameron Dorrat <cdorrat@gmail.com>
2014-03-15 17:14:49 +1100

    start of documentation cleanup

--- modules/tree-config-lein/README.md
+++ modules/tree-config-lein/README.md
@@ -4,10 +4,16 @@ A Clojure library designed to ... well, that part is up to you.
 
 ## Usage
 
-FIXME
+**leiningen**
+Add the following to the dependencies section of project.clj
+
+[tree-config "0.1.0"]
+
+
+TODO: show example 
 
 ## License
 
-Copyright © 2013 FIXME
+Copyright Cameron Dorrat © 2013
 
 Distributed under the Eclipse Public License, the same as Clojure.
--- modules/tree-config-lein/project.clj
+++ modules/tree-config-lein/project.clj
@@ -1,6 +1,6 @@
 (defproject tree-config/lein "0.1.0"
   :description "tree-config support for reading values from leinening profiles or project files"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/cdorrat/tree-config"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
