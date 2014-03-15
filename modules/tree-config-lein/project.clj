e4abdb72fd6e60418fe203fa60dabb4b2157f125
Cameron Dorrat <cdorrat@gmail.com>
2014-03-15 17:03:02 +1100

    removed ref to snapshot version

--- modules/tree-config-lein/project.clj
+++ modules/tree-config-lein/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [tree-config/tree-config "0.1.0-SNAPSHOT"]
+                 [tree-config/tree-config "0.1.0"]
                  [leiningen-core "2.3.4"]]
   :jar-name "tree-config-lein-%s.jar"
   :scm {:name "git"
