21d40fc77f8ca5cc64b83e5bb845856ddfe4748e
Cameron <cameron@ioof>
2015-07-14 16:20:01 +1000

    Moved bouncycastle dependency to :provided section

--- modules/tree-config/project.clj
+++ modules/tree-config/project.clj
@@ -5,8 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0" :scope "provided"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [org.clojure/data.codec "0.1.0"]
-                 [org.bouncycastle/bcprov-jdk15on "1.52"]
-                 [org.bouncycastle/bcpkix-jdk15on "1.52"]]
+                 [org.clojure/data.codec "0.1.0"]]
+  :profiles {:provided
+             {:dependencies [[org.bouncycastle/bcprov-jdk15on "1.52"]
+                             [org.bouncycastle/bcpkix-jdk15on "1.52"]]}} 
   :scm {:name "git"
          :url "https://github.com/cdorrat/tree-config.git"})
