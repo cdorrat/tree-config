580707221b88ef5beb4c9f5a9b6ebcd44c4221bc
Cameron <cnd@hagbard.dorrat.com>
2015-12-21 19:17:25 +1100

    bumped clojure version to 1.7

--- modules/tree-config-jwe/project.clj
+++ modules/tree-config-jwe/project.clj
@@ -1,11 +1,13 @@
-(defproject tree-config/jwe "0.3.2"
+(defproject tree-config/jwe "0.3.4"
   :description "tree-config support for reading JWE formatted encrypted values"
   :url "https://github.com/cdorrat/tree-config"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0" :scope "provided"]
-                 [tree-config/tree-config "0.3.2"]
+  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
+                 [tree-config/tree-config "0.3.4"]
                  [com.nimbusds/nimbus-jose-jwt "3.10"]]
+  :profiles {:provided {:dependencies [[org.bouncycastle/bcprov-jdk15on "1.53"]
+                                       [org.bouncycastle/bcpkix-jdk15on "1.53"]]}}
   :jar-name "tree-config-jwe-%s.jar"
   :scm {:name "git"
          :url "https://github.com/cdorrat/tree-config.git"})
--- modules/tree-config-lein/project.clj
+++ modules/tree-config-lein/project.clj
@@ -1,11 +1,13 @@
-(defproject tree-config/lein "0.3.1"
+(defproject tree-config/lein "0.3.4"
   :description "tree-config support for reading values from leinening profiles or project files"
   :url "https://github.com/cdorrat/tree-config"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0" :scope "provided"]
-                 [tree-config/tree-config "0.1.0"]
+  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
+                 [tree-config/tree-config "0.3.4"]
                  [leiningen-core "2.3.4"]]
+  :profiles {:provided {:dependencies [[org.bouncycastle/bcprov-jdk15on "1.53"]
+                                       [org.bouncycastle/bcpkix-jdk15on "1.53"]]}}
   :jar-name "tree-config-lein-%s.jar"
   :scm {:name "git"
          :url "https://github.com/cdorrat/tree-config.git"}
